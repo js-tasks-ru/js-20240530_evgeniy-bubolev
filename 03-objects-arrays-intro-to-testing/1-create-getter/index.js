0df27529e0b574fc9d5706e1cbb6c8d6c8424db4
Evgenii Bubolev <e.bubolev@raynet.de>
2024-06-02 14:08:44 +0200

    implementing tasks from the 3-rd section

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -1,8 +1,20 @@
 /**
  * createGetter - creates function getter which allows select value from object
- * @param {string} path - the strings path separated by dot
+ * @param {string} propertyPath - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(propertyPath) {
+  const pathParts = propertyPath.split('.');
+
+  return (obj) => {
+    let result = obj;
+    for (const pathPart of pathParts) {
+      result = result[pathPart];
+      if (result === undefined) {
+        return undefined;
+      }
+    }
+    return typeof result === 'function' ? undefined : result;
+  };
 }
