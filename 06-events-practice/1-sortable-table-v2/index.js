9f6e4ae1c921223282f5b4a23a9d434a41c99f69
Evgenii Bubolev <e.bubolev@raynet.de>
2024-06-30 17:13:26 +0200

    Implementing 06-01

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,79 @@
-export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+import SortableTable from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
 
+export default class SortableTableV2 extends SortableTable {
+
+  sorted = null;
+  currentArrow = null;
+
+  constructor(
+    headerConfig, {
+      data = [],
+      sorted = {}
+    } = {}) {
+    super(headerConfig, data);
+    this.sorted = sorted;
+    this.initialize();
+  }
+
+  initialize() {
+    const header = this.element.querySelector('.sortable-table__header');
+    header.querySelectorAll('.sortable-table__cell').forEach(cell => {
+      cell.setAttribute('data-order', 'asc');
+    });
+
+    this.sort(this.sorted.id, this.sorted.order);
+    this.createEventListeners();
+  }
+
+  createEventListeners() {
+    const header = this.element.querySelector('.sortable-table__header');
+    header.addEventListener('click', this.handleHeaderClick.bind(this));
+  }
+
+  destroyEventListeners() {
+    const header = this.element.querySelector('.sortable-table__header');
+    header.removeEventListener('click', this.handleHeaderClick.bind(this));
+  }
+
+  handleHeaderClick = (event) => {
+    event.preventDefault();
+
+    const target = event.target.closest('.sortable-table__cell');
+    if (target?.classList.contains('sortable-table__cell')) {
+
+      if ('false' === target.dataset?.sortable) {
+        return;
+      }
+
+      if (this.currentArrow != null) {
+        this.currentArrow.remove();
+      }
+
+      if (this.sorted.id === target.dataset.id) {
+        this.sorted.order = this.sorted.order === 'asc' ? 'desc' : 'asc';
+        target.dataset.order = this.sorted.order;
+      } else {
+        this.sorted.id = target.dataset.id;
+        this.sorted.order = 'asc';
+      }
+
+      this.sort(this.sorted.id, this.sorted.order);
+
+      const arrow = document.createElement('span');
+      arrow.setAttribute('data-element', 'arrow');
+      arrow.setAttribute('class', 'sortable-table__sort-arrow');
+      const sortArrow = document.createElement('span');
+      sortArrow.setAttribute('class', 'sort-arrow');
+
+      arrow.appendChild(sortArrow);
+      target.appendChild(arrow);
+
+      this.currentArrow = arrow;
+    }
+  }
+
+  destroy() {
+    super.destroy();
+    this.destroyEventListeners();
   }
 }
